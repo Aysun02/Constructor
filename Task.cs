212f657550aee7f656031c52babc2df24c369dd1
Your Name <you@example.com>
2022-05-24 15:04:53 +0400

    Added .

---
+++ Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Person
+    {
+        public Person(int Age)
+        {
+            if (Age >= 18)
+            {
+                IsAdult = true;
+            }
+            else
+            {
+                IsAdult = false;
+            }
+        }
+
+        public string Name;
+        public string Surname;
+        public int Age;
+        public bool IsAdult;
+
+    }
+}
